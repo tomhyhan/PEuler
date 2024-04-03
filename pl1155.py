032dc294bb77003df2151cc0e62d738f9a583cb6
tomhyhan <tomhyhan@gmail.com>
2024-04-03 09:09:28 -0700

    conv pl1155

--- pl1155.py
+++ pl1155.py
@@ -2,17 +2,17 @@ import numpy as np
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         conv = [1 for _ in range(k)]
-        filter = [1 for _ in range(k)]
         mod = 10 ** 9 + 7
         for _ in range(n-1):
-            conv = np.convolve(conv, filter) % mod
-            # print(conv)
-        # print(conv[500+30 - 2: 500+30 + 2])
-        print(conv)
-        print(conv[target-n] )
-        return 0
-
+            new_conv = []
+            for i in range(1,len(conv)+k):
+                left = max(0, i-k)
+                right = min(len(conv), i)
+                new_conv.append(sum(conv[left:right]) % mod)
+            conv = new_conv
+        return conv[target-n]
+    
 s = Solution()
 # s.numRollsToTarget(3,6,6)
-s.numRollsToTarget(1,6,3)
-s.numRollsToTarget(5,30,100)
+s.numRollsToTarget(2,6,3)
+# s.numRollsToTarget(30,30,500)
