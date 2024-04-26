b25c6ae9e6f08214077893fbad161340aaceeb4b
tomhyhan <tomhyhan@gmail.com>
2024-04-26 09:33:32 -0700

    recurse + m

--- pl1105.py
+++ pl1105.py
@@ -1,22 +1,36 @@
+
 class Solution:
     def minHeightShelves(self, books, shelfWidth) -> int:
-
-        self.helper(0,0,books,shelfWidth)
-        pass
+        memo = {}
+        min_height = self.helper(0,0,books,shelfWidth,0,memo)
+        print(memo)
+        print("min_t", min_height)
+        return min_height
     
-    def helper(self, w, h, books, shelfWidth):
-        if len(books) == 0:
-            print(h)
-            return
+    def helper(self, w, h, books, shelfWidth, book_i, memo):
+        key = (book_i, h, w)
+        if len(books) == book_i:
+            return h
+        elif key in memo :
+            return memo[key]
         
-        current_book = books[0]
-        w_c, w_h = current_book
-        current_w = 
+        current_book = books[book_i]
+        w_b, h_b = current_book
+
+        w_current = w + w_b 
+        min_height = float("inf")
+        if w_current <= shelfWidth:
+            h_current = max(h, h_b)
+            c_1 = self.helper(w_current, h_current, books, shelfWidth, book_i+1, memo)
+            min_height = min(min_height, c_1)
+
+        c_2 = self.helper(w_b, h_b, books, shelfWidth, book_i+1, memo) + h
+
+        min_height = min(min_height, c_2)
+        memo[key] = min_height 
+        return min_height
 
 
-        self.helper(w+w_c, h+w_h, books[1:], shelfWidth)
-        
-        pass
-    
 s = Solution()
 s.minHeightShelves([[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], 4)
+# s.minHeightShelves(books = [[1,3],[2,4],[3,2]], shelfWidth = 6)
---
+++ pl1911.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxAlternatingSum(self, nums) -> int:
+        pass
+    
+s = Solution()
+s.maxAlternatingSum([4,2,5,3])
