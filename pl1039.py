b29905552ff9ce788f1a7e84654e4d83969f1c8c
tomhyhan <tomhyhan@gmail.com>
2024-05-07 09:40:53 -0700

    pl1039

--- pl1039.py
+++ pl1039.py
@@ -1,38 +1,46 @@
-from functools import reduce
-from collections import deque
 class Solution:
     def minScoreTriangulation(self, values) -> int:
-        # self.helper(values)
-        print(self.even_area_score([1,2,1,3,1,4], 0))
-        print(self.even_area_score([1,2,1,3,1,4], 1))
-        
-    def even_area_score(self, values, start_idx):
-        # even -> add first
-        # odd -> start with first, add last
-        next_pts = []
-        area = 0
-        n_outer = len(values) // 2
-        # 0 2 4
-        # 1 3 5 
-        for i in range(start_idx, n_outer+start_idx):
-            i = i * 2
-            area += reduce(lambda x, y: x * y, [values[(i + j) % len(values)]  for j in range(3)])
-            next_pts.append(values[i])
-        
-        return area, next_pts
+        score = self.helper(values)
+        print(score)
+        return score
     
-    def odd_area_score(self, values, start_idx):
-
-        pass
     def helper(self, values):
-        if len(values) - 2 == 0:
+        n = len(values)
+        if n - 2 <= 0:
             return 0
-        
-        odd_trig = self.area_score(values)
-        even_trig = self.area_score(values) 
+        min_n = min(values[1:-1])
+        k = values[1:-1].index(min_n) + 1
+
+        area = values[0] * values[-1] * values[k]
+        left = values[0:k+1] 
+        right = values[k:n-1]
+        # area, inner_values = self.get_score(values, k)
+
+        # min_score = min(min_score, area + self.helper(inner_values))
+        print(k)
+        print(area)
+        print(values)
+        print(left, right)
+        return area + self.helper(left) + self.helper(right)
     
+    def get_score(self, values):
+        # 1 3 1 5 1 6
+        # 1 3 1 5 1 6 1
+        n = len(values)
+        n_outer_tris = n // 2
+        start = 0
+        inner_tris = deque([values[start]])
+        area = 0 
+        for i in range(n_outer_tris):
+            idx = i * 2
+            s, m, l = idx, (idx + 1) % n, (idx + 2) % n
+            if l != start:
+                inner_tris.append(values[l])
+            area += values[s] * values[m] * values[l]         
+        return area, inner_tris
     
 s = Solution()
 # s.minScoreTriangulation([1,2,3])
 # s.minScoreTriangulation([3,7,4,5])
 s.minScoreTriangulation([1,3,1,4,1,5])
+# s.minScoreTriangulation([2,2,2,2,1])
