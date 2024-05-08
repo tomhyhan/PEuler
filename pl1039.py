9288c46f70f787f002cb3ab83ac1b75aadec1bbb
tomhyhan <tomhyhan@gmail.com>
2024-05-08 09:14:12 -0700

    pl2110

--- pl1039.py
+++ pl1039.py
@@ -1,46 +1,30 @@
 class Solution:
     def minScoreTriangulation(self, values) -> int:
-        score = self.helper(values)
-        print(score)
-        return score
+        memo = {}
+        min_area = self.helper(0, len(values) - 1, values, memo)
+        # print("min_area", min_area)
+        return min_area
     
-    def helper(self, values):
-        n = len(values)
-        if n - 2 <= 0:
+    def helper(self, left, right, values, memo):
+        key = (left, right)
+        if right-left ==  + 1:
             return 0
-        min_n = min(values[1:-1])
-        k = values[1:-1].index(min_n) + 1
+        elif key in memo:
+            return memo[key]
+         
+        min_area = float('inf')
+        for mid in range(left+1, right):
+            current_area = values[left] * values[right] * values[mid]
+            min_area = min(min_area, current_area + self.helper(left, mid, values, memo) + self.helper(mid, right, values, memo))
 
-        area = values[0] * values[-1] * values[k]
-        left = values[0:k+1] 
-        right = values[k:n-1]
-        # area, inner_values = self.get_score(values, k)
-
-        # min_score = min(min_score, area + self.helper(inner_values))
-        print(k)
-        print(area)
-        print(values)
-        print(left, right)
-        return area + self.helper(left) + self.helper(right)
+        memo[key] = min_area
+        return min_area
     
     def get_score(self, values):
-        # 1 3 1 5 1 6
-        # 1 3 1 5 1 6 1
-        n = len(values)
-        n_outer_tris = n // 2
-        start = 0
-        inner_tris = deque([values[start]])
-        area = 0 
-        for i in range(n_outer_tris):
-            idx = i * 2
-            s, m, l = idx, (idx + 1) % n, (idx + 2) % n
-            if l != start:
-                inner_tris.append(values[l])
-            area += values[s] * values[m] * values[l]         
-        return area, inner_tris
+        pass
     
 s = Solution()
-# s.minScoreTriangulation([1,2,3])
-# s.minScoreTriangulation([3,7,4,5])
+s.minScoreTriangulation([1,2,3])
+s.minScoreTriangulation([3,7,4,5])
 s.minScoreTriangulation([1,3,1,4,1,5])
-# s.minScoreTriangulation([2,2,2,2,1])
+s.minScoreTriangulation([2,2,2,2,1])
---
+++ pl2110.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getDescentPeriods(self, prices) -> int:
+        dp = [0] * len(prices)
+        dp[0] = 1
+        prev_p = prices[0]
+        for i in range(1, len(prices)):
+            current_p = prices[i]
+            if prev_p - current_p == 1:
+                dp[i] = dp[i-1] + 1
+            else:
+                dp[i] = 1
+            prev_p = current_p
+        return sum(dp)
+s = Solution()
+s.getDescentPeriods([3,2,1,4])
+    
