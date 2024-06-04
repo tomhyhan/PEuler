bbeed83b091d74543342408a976b6bfb289d358d
tomhyhan <tomhyhan@gmail.com>
2024-06-04 10:29:06 -0700

    jh

---
+++ jh.py
@@ -0,0 +1,8 @@
+def solution(land, P, Q):
+    # use binary search for remove <-> install
+    # 1000000000
+    answer = -1
+    return answer
+
+solution([[1, 2], [2, 3]], 3, 2)
+solution([[4, 4, 3], [3, 2, 2], [ 2, 1, 0 ]], 5, 3)
