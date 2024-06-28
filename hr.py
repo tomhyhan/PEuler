ec0819d2ca0a879ff8592310cbab93f65212f7b6
tomhyhan <tomhyhan@gmail.com>
2024-06-28 09:31:07 -0700

    ch

--- hr.py
+++ hr.py
@@ -1,32 +1,34 @@
+import sys
+
+sys.setrecursionlimit(9999)
+
 class RoomInfo:
     def __init__(self, max_key, idx):
         self.max_key = max_key
         self.idx = idx
 
-def update_key(room_info, ckey):
-    if room_info[ckey] is None:
-        pass
-    else:
-        pass
+    def __repr__(self):
+        return f"{self.max_key} {self.idx}"
 
+def find_empty_room(rn, room_info):
+    if rn not in room_info:
+        return rn
+    else:
+        room_info[rn].max_key = find_empty_room(room_info[rn].max_key, room_info)
+        return room_info[rn].max_key
+    
 def solution(k, room_number):
     room_info = {}
     
     for idx, rn in enumerate(room_number):
         if rn not in room_info:
-            ckey = rn + 1
-            # if ckey already exist update key
-            # 1 1 2 3 4 5 6 7 1 5
-            if ckey in room_info:
-                update_key(room_info, ckey)
             room_info[rn] = RoomInfo(rn+1, idx)
-            
-            
         elif rn in room_info:
             # find next empty room
-            next_room = room_info[rn].max_key
-            room_info[next_room] = RoomInfo(next_room + 1, idx)
-            pass
+            empty_room = find_empty_room(rn, room_info)
+            room_info[empty_room] = RoomInfo(empty_room + 1, idx)
+
+    return [ri[0] for ri in sorted(room_info.items(), key=lambda x: x[1].idx)]
 
 
 # [1,3,4,2,5,6]
