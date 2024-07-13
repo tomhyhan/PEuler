503328772931ab1f9d2fe221f4df74fb5f440b63
tomhyhan <tomhyhan@gmail.com>
2024-07-13 11:29:58 -0700

    sy

--- sy.py
+++ sy.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 def solution(n, costs):
     graph = defaultdict(list)
@@ -7,22 +7,32 @@ def solution(n, costs):
         graph[s].append((e,c))
         graph[e].append((s,c))
 
-    visited = {}
-    stack = [(0, 0)]
-    start = 0
-    while stack:
-        node, current_c = stack.pop()
+    # visited = {}
+    min_cost = float("inf")
+    for node in range(n):
+        queue = deque([(node, 0)])
+        visited = {}
+        total_cost = 0 
         
-        if node == start and len(visited) == n:
-            print(current_c)
-            break
-        
-        if node in visited:
-            continue
-        visited[node] = current_c
-        
-        for dest, next_c in graph[node]:
-            stack.append((dest, current_c + next_c))
+        while queue:
+            current_n, cost = queue.popleft()
 
-            
-solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,8],[2,3,1]])
+            if current_n in visited:
+                total_cost = current_n
+                continue
+            visited.add(current_n)
+
+            for nnode, ncost, in graph[current_n]:
+                queue.append((nnode,cost + ncost))                
+        min_cost = min(min_cost, total_cost)
+
+    print(min_cost)
+    return min_cost            
+
+# solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,8],[2,3,1]])
+# solution(7, [[2, 3, 7], [3, 6, 13], [3, 5, 23], [5, 6, 25], [0, 1, 29], [1, 5, 34], [1, 2, 35], [4, 5, 53], [0, 4, 75]])
+# solution(5,[[0, 1, 1], [3, 4, 1], [1, 2, 2], [2, 3, 4]])
+# solution(5, [[0, 1, 1], [3, 1, 1], [0, 2, 2], [0, 3, 2], [0, 4, 100]])
+# solution(5,[[0, 1, 1], [0, 2, 2], [0, 3, 3], [0, 4, 4], [1, 3, 1]])
+# solution(5,[[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 3], [2, 3, 8], [3, 4, 1]])
+solution(6,[[0, 1, 5], [0, 3, 2], [0, 4, 3], [1, 4, 1], [3, 4, 10], [1, 2, 2], [2, 5, 3], [4, 5, 4]])
