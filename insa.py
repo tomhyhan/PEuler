967e73ed095ab4f4a9379ece49f8af88f0c87fc6
tomhyhan <tomhyhan@gmail.com>
2024-08-06 09:57:08 -0700

    110

--- insa.py
+++ insa.py
@@ -1,28 +1,23 @@
 def solution(scores):
     
+    won_f, won_s = scores[0]
     scores.sort(key= lambda x: (-x[0], x[1]))
-    print(scores)    
     
-    new_scores = [scores[0]]
-    minb = scores[0][1]
-    for i in range(1, len(scores)):
-        pass
-    # won_f, won_s = scores[0]
+    rank = 0 
+    maxb = float("-inf")
+    for i in range(len(scores)):
+        a, b = scores[i]
+        
+        if won_f < a and won_s < b:
+            return -1
+        
+        if b >= maxb:
+            maxb = b
+            if a + b > won_f + won_s:
+                rank += 1
     
-    # for i in range(len(scores)):
-    #     scores[i] = [sum(scores[i]), *scores[i]]
+    return rank + 1
 
-    # scores.sort(reverse=True)
-    # print(scores)
-    # for i, score in enumerate(scores):
-    #     _, f, s = score
-        
-    #     if f == won_f and s == won_s:
-    #         return i + 1
-        
-    #     if f > won_f and s > won_s:
-    #         return -1
     
-    # raise Exception()
 
 print(solution([[2,2],[1,4],[3,2],[3,2],[2,1]]))
