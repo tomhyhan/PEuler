e7dcc863bca2e9cbb02e6eb4eb2d525c09fd863c
tomhyhan <tomhyhan@gmail.com>
2024-08-07 12:17:28 -0700

    100

--- 110.py
+++ 110.py
@@ -1,23 +1,29 @@
 import re
 
 def solution(s):
-    print("102010".find("20"))
-    # for i in range(len(s)):
-    #     a = s[i]
-    #     # print(find)
-    #     while True:
-    #         find = re.search(r'11+0', a)
-    #         print(find)
-    #         if not find or len(find.group()) <= 3:
-    #             break
-    #         b, e = find.span()
-    #         print(a, b, e)
-    #         a_li = list(a)
-    #         a_li[b+2], a_li[e-1] = '0', '1'
-    #         a = ''.join(a_li)
-    #     print("a", a)        
-    # print(s)
-    # return s
+    result = []
+    for i in range(len(s)):
+        a = list(s[i])
+        helper(a, 0)
+        # print(a)
+        result.append(''.join(a))
+    print(result)
+    return result
 
-["1101","100110110","0110110111"]
+def helper(a, l):
+    joined = ''.join(a[l:])
+    if not (find := re.search(r'111+0', joined)):
+        return
+    b, e = find.span()
+    a[l+b+2], a[l+e-1] = a[l+e-1], a[l+b+2]
+    helper(a, l+b+3)
+
+# ["1101","100110110","0110110111"]
 solution(["1110","100111100","0111111010"])
+
+# 0110110110110101
+
+
+# 0101101101101101
+# 1100110110110101
+solution(["0110111011101001"])
