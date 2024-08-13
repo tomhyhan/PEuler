9cd4f7c0e7da8077de48b535cde75d5efe7d627a
tomhyhan <tomhyhan@gmail.com>
2024-08-13 13:49:40 -0700

    stars

--- stars.py
+++ stars.py
@@ -1,12 +1,32 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def solution(a):
-    stack = []
+    
+    counter = Counter(a)
+    longest = 0
+    
+    for num, cnt in counter.items():
+        if longest > cnt:
+            continue
+        
+        i=0
+        stars = 0
+        while i < len(a)-1:
+            if a[i] != a[i+1] and (a[i] == num or a[i+1] == num):
+                i += 2
+                stars += 1
+                continue
+            i += 1
+        longest = max(longest, stars)
+    # print(longest)
+    return longest * 2
 
-    for i in range(len(a)):
+
+def solution(a):
         
-        
-        pass
+    pass
+
+
 
 # 0110
 # 110
@@ -15,5 +35,5 @@ def solution(a):
     
 # solution([0,3,3,0,7,2,0,2,2,0,0])
 solution([0,3,3,0,7,2,0,2,2,0])
-# solution([5,2,3,3,5,3])
+solution([5,2,3,3,5,3])
 # solution([0])
