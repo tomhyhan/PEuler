e64a9c52e9b7baaf604ff0015a007056f170d2cd
tomhyhan <tomhyhan@gmail.com>
2024-08-22 10:43:46 -0700

    cntd

--- cntd.py
+++ cntd.py
@@ -1,22 +1,29 @@
 from collections import deque
-
+#  one more with point target reverse
+#  find optimal
 def solution(target):
-    bulls, left = divmod(target, 50)
-    answer = [bulls, bulls]
-
-    if left <= 20:
-        answer[0] +=1
-        answer[1] +=1
-    else:
-        if (left % 2 == 0 and left <= 40) or (left % 3 == 0):
-            answer[0] += 1
-        else:
-            answer[0] +=2
-            answer[1] +=2
-    print(answer)
-    return answer
+    result = [[float("inf"),0] for _ in range(target+1)]
+    singles = [s for s in range(1,21)]
+    doubles = [s*2 for s in range(1,21)]
+    triples =[s*3 for s in range(1,21)]
+    points = singles + doubles + triples + [50]
+    # print(points)
+    result[0][0] = 0
+    for i, p in enumerate(points):
+        for s in range(target+1):
+            if s >= p:
+                new_result = [r for r in result[s-p]]
+                new_result[0] += 1
+                if 0 <= i < 20 or  p == 50: 
+                    new_result[1] += 1
+                print(s,p, new_result)
+                result[s] = min(result[s], new_result, key=lambda x: (x[0], -x[1]))
+    
+    return result[target]
+    print(result)
             
             
-solution(21)
-solution(58)
-solution(23)
+# solution(2)
+# solution(21)
+# solution(58)
+# solution(23)
