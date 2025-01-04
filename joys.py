f2894f7658bea124493dac082827703ae22e16c8
tomhyhan <tomhyhan@gmail.com>
2025-01-04 10:10:43 -0800

    joys

--- joys.py
+++ joys.py
@@ -1,33 +1,58 @@
 # (13 - abs(ord(name[i]) - ord('N')))
 
-def ways(name, n_As, back):
-    total = 0
-    for i in range(len(name)):
-        curr_name = name[-i] if back else name[i]
-        total += (13 - abs(ord(curr_name) - ord('N')))
-        if len(name) - i == n_As:
-            total -= 1
-            break
-        
-        if i < len(name) - 1:
-            total += 1
-        
-        if curr_name == 'A':
-            n_As -= 1
-    return total
+def ways(i, moves, go_front, name):
+    
+    if name[i] != 'A':
+        ways(i + 1, moves + 1, go_front, name)
+    pass
 
 def solution(name):
-    n_As = name.count('A')
+    moves = 0
+    min_move = len(name) - 1
     
-    if n_As == len(name):
-        return 0
-    
-    ftotal = ways(name, n_As, False)
-    btotal = ways(name, n_As, True)
-    return ftotal if ftotal < btotal else btotal
+    for i in range(len(name)):
+        moves += (13 - abs(ord(name[i]) - ord('N')))
+        
+        next = i + 1
+        cnt = 0
+        while next < len(name) and name[next] == 'A':
+            cnt += 1
+            next += 1
+            
+        min_move = min(min_move, i + len(name) - 1- cnt, i + 2 * (len(name) - next))
+    moves += min_move
+    return moves
 
-print(solution("BABA"))
-# solution("JAN")
+# 0 8 16
+# 0 9 1
+
+# 1 9 15
+# 2 9 2
+
+# 2 10 14
+# 4 9 3
+
+# 3 7 5
+# 6 9 8
+
+# 4 9 6
+# 8 9 8
+
+# 5 11 7
+# 10 9 8
+
+# 6 13 8
+# 12 9 8
+
+# 7 15 9
+# 14 9 8
+
+# 8 16 8
+# 16 9 9
+
+# 10
+# print(solution("BBBBAAAAB"))
+solution("JAN")
 # solution("JAZ")
 # solution("JZA")
 # solution("JEROEN")
