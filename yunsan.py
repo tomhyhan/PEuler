c6358135a2b91839e7cd3eed6fdf1c8796819e88
tomhyhan <tomhyhan@gmail.com>
2025-01-14 10:40:29 -0800

    yunsan

--- yunsan.py
+++ yunsan.py
@@ -1,31 +1,26 @@
-def helper(tokens):
-    if len(tokens) == 1:
-        return tokens[0]
-    
-    max_sum = float('-inf')
-    
-    for i in range(1, len(tokens)):
-        left = helper(tokens[:i])
-        right = helper(tokens[i:])
-        current = left + right
-        max_sum = max(max_sum, current)
-    
-    return max_sum
-
 def solution(arr):
-    # Parse numbers and operators
     numbers = [int(arr[i]) for i in range(0, len(arr), 2)]
-    operators = [arr[i] for i in range(1, len(arr), 2)]
+    ops = [arr[i] for i in range(1, len(arr), 2)]
     
-    # Create tokens with proper signs
-    tokens = [numbers[0]]
-    for i in range(len(operators)):
-        if operators[i] == '+':
-            tokens.append(numbers[i + 1])
-        else:  # '-'
-            tokens.append(-numbers[i + 1])
-    print(helper(tokens))
-    return 
+    print(numbers, ops)
+    # [1, 3, 5, 8] ['-', '+', '-']
+    N = len(numbers)
+    MAXD = [[float("-inf") if i!=j else numbers[i] for j in range(N)] for i in range(N)]
+    MIND = [[float("inf") if i!=j else numbers[i] for j in range(N)] for i in range(N)]
+
+    for brac in range(N):
+        for i in range(N-brac):
+            j = i + brac
+            for k in range(i, j):
+                if ops[k] == '+':
+                    MAXD[i][j] = max(MAXD[i][j], MAXD[i][k] + MAXD[k+1][j])
+                    MIND[i][j] = min(MIND[i][j], MIND[i][k] + MIND[k+1][j])
+                elif ops[k] == '-':
+                    MAXD[i][j] = max(MAXD[i][j], MAXD[i][k] - MIND[k+1][j])
+                    MIND[i][j] = min(MIND[i][j], MIND[i][k] - MAXD[k+1][j])
+    
+    return MAXD[0][-1]
+
 # (((1 - 3) + 5) - 8) = -5
 # ((1 - (3 + 5)) - 8) = -15
 # (1 - ((3 + 5) - 8)) = 1
@@ -34,5 +29,5 @@ def solution(arr):
 
 # 1 -3 +5 -8, (1-3)+5-8 | 1-(3+5)-8 | 1-3+(5-8), (-2+5)-8 | -2+(5-8) 
 solution(["1", "-", "3", "+", "5", "-", "8"])
-solution(["5", "-", "3", "+", "1", "+", "2", "-", "4"])
+# solution(["5", "-", "3", "+", "1", "+", "2", "-", "4"])
 # solution(["5", "-", "3", "+", "1", "+", "2", "-", "4", '+', "5", "-", "3", "+", "1", "+", "2", "-", "4", '+', "5", "-", "3", "+", "1", "+", "2", "-", "4", '+', "5", "-", "3", "+", "1", "+", "2", "-", "4"])
