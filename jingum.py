f7dd9b62215822a979d7056913ebc7d194fef98f
tomhyhan <tomhyhan@gmail.com>
2025-01-23 12:18:07 -0800

    jingum

--- jingum.py
+++ jingum.py
@@ -1,29 +1,35 @@
 def solution(distance, rocks, n):
     # 각 지점 사이의 거리의 최솟값 중에 가장 큰 값
-    max_distance = 1_000_000_000
+    rocks = [0] + rocks + [distance]
     rocks.sort()
-    
+
     n_rocks = len(rocks)
-    left = 0
-    right = max_distance
-    print(rocks)
-    max_conn_distance = float('-inf')    
+    left = 1
+    right = distance
+    min_distance = float('inf')
+
     while left <= right:
         mid = (left + right) // 2
-        
-        cnt_n = 0
-        for i in range(n_rocks):
-            distance_btw = distance if i == n_rocks-1 else rocks[i+1] - rocks[i]
-            if mid > distance_btw:
-                cnt_n += 1
-        
-        if cnt_n >= n:
-            right = mid - 1
-            if cnt_n == n:
-                max_conn_distance = max(max_conn_distance, mid)
-        else:
-            left = mid + 1
-        
-    return max_conn_distance
 
-print(solution(25, [2, 14, 11, 21, 17], 2))
+        cnt_n = 0
+        curr_rock = rocks[0]
+        min_diff = float('inf')
+        for i in range(1, n_rocks):
+            next_rock = rocks[i]
+            diff = next_rock - curr_rock 
+            if diff < mid:
+                cnt_n += 1
+            else:
+                curr_rock = next_rock
+                min_diff = min(min_diff, diff)
+        print(left, right)
+        if cnt_n > n:
+            right = mid - 1
+        else:
+            min_distance = mid
+            left = mid + 1
+
+    return min_distance
+
+# print(solution(25, [2, 14, 11, 21, 17], 2))
+print(solution(25, [2, 4, 6, 8, 10], 2))
