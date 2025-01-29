4bd4b5690dc1a8201f8020ff88b3c78e9c7c33d9
tomhyhan <tomhyhan@gmail.com>
2025-01-29 11:55:38 -0800

    romn

--- romn.py
+++ romn.py
@@ -1,28 +1,31 @@
-from collections import deque
+from collections import defaultdict
 
 def solution(arrows):
     DIRS = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
     
-    a_map = set()
-    seen = set()
-    
+    a_map = defaultdict(set)
     n_rooms = 0
+    row, col = 0, 0
     
-    r, c = 0, 0
     for arrow in arrows:
-        nr = r + DIRS[arrow][0]
-        nc = c + DIRS[arrow][1]
-        a_map.add(((r,c), (nr,nc)))
-        
-        seen.add((r,c))
-        
-        if (nr, nc) in seen:
-            n_rooms += 1
-        
-        r = nr
-        c = nc
+        for _ in range(2):
+            nrow = row + DIRS[arrow][0]
+            ncol = col + DIRS[arrow][1]
+
+            prev = (row, col)
+            curr = (nrow, ncol)
+            if curr in a_map and prev not in a_map[curr]:
+                n_rooms += 1
+            a_map[curr].add(prev)            
+            a_map[prev].add(curr)            
+
+            row = nrow
+            col = ncol
     
     # print(n_rooms)
     return n_rooms
 
-solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0])
+# print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
+print(solution([5, 2, 7, 1, 6, 3]))
+print(solution([6, 5, 2, 7, 1, 4, 2, 4, 6]))
+print(solution([6, 2, 4, 0, 5, 0, 6, 4, 2, 4, 2, 0] ))
